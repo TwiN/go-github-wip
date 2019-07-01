b15d8d6834baa7304598c95a7e1c5daebfd24997
TwinProduction <twin@twinnation.org>
2019-07-01 15:49:18 -0400

    Add logs for creation of github client

--- util/github.go
+++ util/github.go
@@ -20,6 +20,7 @@ func GetGithubClient(appId, installationId int64) (*github.Client, context.Conte
 }
 
 func createGithubClient(appId, installationId int64) *github.Client {
+	log.Printf("[createGithubClient] Creating client for appId=%d and installationId=%d\n", int(appId), int(installationId))
 	transport := http.DefaultTransport
 	itr, err := ghinstallation.NewKeyFromFile(transport, int(appId), int(installationId), os.Getenv("GO_GITHUB_WIP_APP_PRIVATE_KEY"))
 	if err != nil {
